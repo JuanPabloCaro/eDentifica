f649a3311a553b77f46af3c1b727f6cae669b0fb
jcaro <jcaropenuela@gmail.com>
2024-03-14 20:56:11 +0100

    Commit Init

---
+++ src/main/java/com/project/edentifica/EdentificaApplication.java
@@ -0,0 +1,13 @@
+package com.project.edentifica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdentificaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdentificaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/edentifica/EdentificaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.edentifica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdentificaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
