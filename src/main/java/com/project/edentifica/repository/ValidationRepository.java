ead8399466dccb4ef51271befc41d11a09bddbf2
jcaro <jcaropenuela@gmail.com>
2024-05-04 13:32:21 +0200

    Commit Eliminacion validation repositorio y servicio

--- src/main/java/com/project/edentifica/repository/ValidationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.edentifica.repository;
-
-import com.project.edentifica.model.Validation;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ValidationRepository extends MongoRepository<Validation,String> {
-}
